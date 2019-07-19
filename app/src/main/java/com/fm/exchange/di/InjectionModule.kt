8f091d887bde5fc78710a334d7d779e727d8598e
Faisal Mohammed <faisalmohd83@gmail.com>
2019-07-19 16:59:10 +0100

    Commit 04

--- app/src/main/java/com/fm/exchange/di/InjectionModule.kt
+++ app/src/main/java/com/fm/exchange/di/InjectionModule.kt
@@ -1,10 +1,10 @@
 package com.fm.exchange.di
 
-import com.fm.excahnge.util.GenericUtilsImpl
-import com.fm.excahnge.util.image.ImageFetcherUtilImpl
-import com.fm.excahnge.util.number.NumberFormatterUtilImpl
-import com.fm.exchange.common.RetrofitFactory
-import com.fm.exchange.network.ExchangeRepository
+import com.fm.exchange.data.ExchangeRepository
+import com.fm.exchange.network.RetrofitFactory
+import com.fm.exchange.util.GenericUtilsImpl
+import com.fm.exchange.util.image.ImageFetcherUtilImpl
+import com.fm.exchange.util.number.NumberFormatterUtilImpl
 import com.fm.exchange.view.ExchangeActivity
 import com.fm.exchange.view.ExchangeAdapter
 import com.fm.exchange.view.ExchangeViewModel
@@ -12,21 +12,30 @@ import com.fm.exchange.view.OnListItemTapListener
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-var diInjectionModule = module {
-
-    // Adapter
-    single { ExchangeAdapter(ExchangeActivity() as OnListItemTapListener) }
+var viewModule = module {
 
     // ViewModel
     viewModel { ExchangeViewModel() }
 
+    // Adapter
+    factory { ExchangeAdapter(ExchangeActivity() as OnListItemTapListener) }
+}
+
+var dataModule = module {
+
     // Repository
     single { ExchangeRepository(get()) }
+}
+
+var utilsModule = module {
 
     // Utils
     single { GenericUtilsImpl() }
     single { ImageFetcherUtilImpl() }
     single { NumberFormatterUtilImpl() }
+}
+
+var networkModule = module {
 
     // Retrofit
     single { RetrofitFactory() }
